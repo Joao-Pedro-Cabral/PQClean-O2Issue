e1d645da6e7f3cacf1615051941819494aa0ac46
Joao-Pedro-Cabral <miranda.jp@usp.br>
2025-09-03 14:48:45 +0100

    Simplifying even more debug code

--- bug/bug.c
+++ bug/bug.c
@@ -1,19 +1,24 @@
 
-#include "inner.h"
-#include "randombytes.h"
+#include <stdint.h>
+#include <stdio.h>
 
 int main() {
-  sampler_context spc;
+  uint8_t tmp[56];
+  uint32_t d32[14];
+  for (int i = 0; i < 56; i ++) {
+      tmp[i] = 37*i;
+  }
 
-  unsigned char seed[48];
-  inner_shake256_context sc;
-
-  randombytes(seed, sizeof seed);
-  inner_shake256_init(&sc);
-  inner_shake256_inject(&sc, seed, sizeof seed);
-  inner_shake256_flip(&sc);
-
-  int logn = 9;
-  spc.sigma_min = fpr_sigma_min[logn];
-  PQCLEAN_FALCON512_CLEAN_prng_init(&spc.p, &sc);
+  for (int i = 0; i < 14; i ++) {
+      uint32_t w;
+      w = (uint32_t)tmp[(i << 2) + 0]
+          | ((uint32_t)tmp[(i << 2) + 1] << 8)
+          | ((uint32_t)tmp[(i << 2) + 2] << 16)
+          | ((uint32_t)tmp[(i << 2) + 3] << 24);
+      d32[i] = w;
+  }
+  for (int i = 0; i < 14; i ++) {
+      printf("%d\n", d32[i]);
+  }
+  return 0;
 }
